6f1507308c11e5e58a784fcbe3f6cf3e52d67fe5
jam54 <git@jam54.com>
2022-05-28 18:34:10 +0200

    Added the Ubuntu font

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,6 +1,9 @@
 const root = document.getElementById("root");
 root.className = "landingpage";
 
+let description = document.createElement("h1");
+description.innerHTML = "Almost before we knew it, we had left the ground.";
+
 let folderPicker = document.createElement("folder-picker");
 
-root.append(folderPicker);
+root.append(description, folderPicker);
