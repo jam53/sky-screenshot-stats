6c42b82fc05772d0a8fa411edfac768a1c714abe
jam54 <git@jam54.com>
2022-05-29 14:52:26 +0200

    Finished the landingpage

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,9 +1,2 @@
 const root = document.getElementById("root");
-root.className = "landingpage";
 
-let description = document.createElement("h1");
-description.innerHTML = "Sky Screenshot Stats";
-
-let folderPicker = document.createElement("folder-picker");
-
-root.append(description, folderPicker);
