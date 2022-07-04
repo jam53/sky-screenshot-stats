f747df727aa35e02f1ef9afcf8d75906f9726991
jam54 <git@jam54.com>
2022-07-04 22:31:55 +0200

    Fixed a typo

--- js/stats.js
+++ js/stats.js
@@ -32,7 +32,7 @@ div.innerHTML =
         <div class="statsNumbers">
             <svg width="35px" height="35px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <h5>
-                Your longest playsession lasted <em> ` + millisecondsToTime(generatedData.maxMillisecondsOnADay) + ` </em>
+                Your longest play session lasted <em> ` + millisecondsToTime(generatedData.maxMillisecondsOnADay) + ` </em>
             </h5>
         </div>
     </div>
