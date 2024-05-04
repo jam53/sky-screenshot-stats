df5dcf111410551429e4ffda90039e2870d4232b
jam54 <git@jam54.com>
2024-05-04 21:46:26 +0200

    Fix issue where an icon's fill color wasn't transparent on the `stats.html` page

--- js/stats.js
+++ js/stats.js
@@ -24,7 +24,7 @@ div.innerHTML =
             </h5>
         </div>
         <div class="statsNumbers">
-            <svg width="35px" height="35px" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+            <svg width="35px" height="35px" stroke="currentColor" fill="transparent" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
             <h5>
                 The most amount of screenshots you took on one day was <em> ` + (new Intl.NumberFormat()).format(generatedData.maxScreenshotsOnADay) + ` </em>
             </h5>
