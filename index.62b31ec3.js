422fe00b519320d9cd15040ad650eb694d5c82f8
jam54 <git@jam54.com>
2024-11-30 19:20:08 +0100

    Publish version 1.1.13

--- index.62b31ec3.js
+++
@@ -1,2 +0,0 @@
-!function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)r[e=o[i]]||(r[e]=n)}();
-//# sourceMappingURL=index.62b31ec3.js.map
